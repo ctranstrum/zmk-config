b163fd53ad931347ed6540a2ff59aa26a8aed501
Chad Transtrum <chad@transtrum.net>
2024-02-22 19:57:44 -0700

    fix syntax error

--- config/boards/shields/dongle_display/widgets/modifiers.c
+++ config/boards/shields/dongle_display/widgets/modifiers.c
@@ -68,7 +68,7 @@ struct modifier_symbol ms_cmd = {
 
 struct modifier_symbol *modifier_symbols[] = {
     // this order determines the order of the symbols
-    &ms_shift
+    &ms_shift,
     &ms_control,
     &ms_opt,
     &ms_cmd,
